fd835e443475f3b97315b21f607db0be56a000f2
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-13 23:24:13 +0000

    se agregaron nuevas variables, para ejecutar un mensaje concatenado

--- class1/Tipodatos.java
+++ class1/Tipodatos.java
@@ -1,7 +1,12 @@
 public class Tipodatos {
     public static void main(String[] args){
         long telefono = 3052859085L;
-        System.out.println("mi numero es: "+ telefono);
+        int Edad = 25;
+        double altura = 1.75;
+        String Nombre = "juan";
+        boolean estudiante = true;
+
+        System.out.println("Hola mi nombre es " + Nombre + ", tengo " + Edad + " años" + " mido " + altura + " y soy estudiante " + estudiante );
 
     }
 }
