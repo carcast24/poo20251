758b0a17fa81ea93be5d736cc7add74ed9cb8a85
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-13 23:54:03 +0000

    se agregaron comentarios

---
+++ class2/Tipodatos.java
@@ -0,0 +1,13 @@
+public class Tipodatos {
+    public static void main(String[] args) {
+        //Tipos de datos
+        long telefono = 3052859085L; // entero extenso
+        int Edad = 25; //Enteros simple
+        double altura = 1.75; // dato de tipo Real (double y float)
+        String Nombre = "juan"; // cadenas de caracteres 
+        boolean estudiante = true; // booleanos devuelve valores true or false 
+
+        System.out.println("Hola mi nombre es " + Nombre + ", tengo " + Edad + " años" + " mido " + altura + " y soy estudiante " + estudiante + " mi telefono es " + telefono);
+
+    }
+}
