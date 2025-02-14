33d06c219ac01158be969e1ff7d9b1107dd327e3
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-14 01:36:40 +0000

    se agrego while con decremento en uno

--- class2/Ciclos.java
+++ class2/Ciclos.java
@@ -17,6 +17,13 @@ public class Ciclos {
             System.out.println("ciclo j numero: " + j);
             j++;
         }
+        // aplique while para hacer un decremento, (su proceso es exactamente igual )
+
+        int g = 10; // inicia en 
+        while(g >= 1)// termina en{
+            System.out.println("decremento g en: " + g);
+            g--;// incremento o decremento
+        }
 
 
 
