5f56936f749cbfed2ea95feb2d0994987979dbd2
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-14 01:27:40 +0000

    se agrega bucle while

--- class2/Ciclos.java
+++ class2/Ciclos.java
@@ -1,10 +1,24 @@
 public class Ciclos {
     public static void main(String[] args){
-        //  inicia en- termina-incremento
-        //    ↓           ↓      ↓
-        for(int i = 1; i <= 5; i++) {
+        //  inicia en; termina; incremento o decremento
+        //    ↓           ↓       ↓
+        for(int i = 1;  i <= 5;  i++) {
             System.out.println("ciclo numero: " + i);
         }
+        /* estructura del while
+         * inicia en;
+         * while(condicion){
+         * ---------------
+         * incremendo o decremento
+         * }
+         */
+        int j = 1;
+        while(j <= 5){
+            System.out.println("ciclo j numero: " + j);
+            j++;
+        }
+
+
 
     }
 }
