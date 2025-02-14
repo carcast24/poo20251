003e744c7ce466240d5117c1522817a7cbd13043
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-14 01:09:24 +0000

    se crea bucle for estructura basica

---
+++ class2/Ciclos.java
@@ -0,0 +1,10 @@
+public class Ciclos {
+    public static void main(String[] args){
+        //  inicia en- termina-incremento
+        //    ↓           ↓      ↓
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("ciclo numero: " + i);
+        }
+
+    }
+}
---
+++ class2/Condicionales.java
@@ -0,0 +1,14 @@
+public class Condicionales {
+    public static void main(String[] args) {
+        int numero = -10;
+
+        if(numero > 0){
+            System.out.println("El numero es positivo");
+        } else if(numero < 0){
+            System.out.println("El numero es negativo");
+
+        } else{
+            System.out.println("El numero es cero");
+        }
+    }
+}
