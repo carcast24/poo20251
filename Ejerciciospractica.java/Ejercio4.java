6598c13635a41f59e855f26f822b1fc7b138bd29
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-26 00:49:40 +0000

    creacion de clase, invocacion de clase,  definicion de arreglos, impresion de arreglos vacios

---
+++ Ejerciciospractica.java/Ejercio4.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Ejercio4 {
+    public static void main(String[] args) {
+        /* Analisis: hacer dos arreglos con edades de dos grupos de personas, luego encontrar el mayor */
+        int [] edad1 = new int[5]; // declaro areglos con sus dimenciones 
+        int [] edad2 = new int[5];
+        System.out.println(Arrays.toString(edad1)); // imprimo arreglos para ver sus cambios 
+        System.out.println(Arrays.toString(edad2));
+    }
+}
