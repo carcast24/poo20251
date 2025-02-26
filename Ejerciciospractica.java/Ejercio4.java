c4154f717e9e0fb2e91393fb69e8a9b04370e904
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-02-26 00:53:31 +0000

    llenar array 1 de forma manual

--- Ejerciciospractica.java/Ejercio4.java
+++ Ejerciciospractica.java/Ejercio4.java
@@ -2,10 +2,22 @@ import java.util.Arrays;
 
 public class Ejercio4 {
     public static void main(String[] args) {
-        /* Analisis: hacer dos arreglos con edades de dos grupos de personas, luego encontrar el mayor */
-        int [] edad1 = new int[5]; // declaro areglos con sus dimenciones 
-        int [] edad2 = new int[5];
-        System.out.println(Arrays.toString(edad1)); // imprimo arreglos para ver sus cambios 
+        /*
+         * Analisis: hacer dos arreglos con edades de dos grupos de personas, luego
+         * encontrar el mayor
+         */
+        int[] edad1 = new int[5]; // declaro areglos con sus dimenciones
+        // lleno el primer arreglo manual
+        edad1 [0] = 20;
+        edad1 [1] = 50;
+        edad1 [2] = 17;
+        edad1 [3] = 13;
+        edad1 [4] = 10;
+        int[] edad2 = new int[5];
+        System.out.println(Arrays.toString(edad1)); // imprimo arreglos para ver sus cambios
         System.out.println(Arrays.toString(edad2));
+
+        
+
     }
 }
