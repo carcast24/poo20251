56966618e7f0846b8cab5367c70f8b27621303d4
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-03-06 22:23:53 +0000

    creacion de la clase moto

---
+++ MiniProyecto.java/Moto.java
@@ -0,0 +1,12 @@
+public class Moto {
+    private String placa;
+    private int cilindrada;
+    private short horaIngreso; // uso shor ya que el intervalo que usa es el justo y ademas solo usa 2bytes de almacenamiento
+
+    public Moto(String placa, int cilindrada, short horaIngreso){
+        this.placa = placa;
+        this.cilindrada = cilindrada;
+        this.horaIngreso = horaIngreso;
+
+    }
+}
