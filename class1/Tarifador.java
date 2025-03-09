99c8b31b8ba53675624acf4eae21a3a9056a8b95
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-03-09 11:56:02 -0500

    se crea clase tarifador, se invoca los metodos de clase scanner

---
+++ class1/Tarifador.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tarifador{
+    public static void  main (String[] args){
+        Scanner; // invoco la clase scanner, me permite que el usuario ingrese datos al programa
+
+
+    }
+}
