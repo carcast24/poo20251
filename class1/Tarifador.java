f2f6b4d9acae4e42ca40c7b4e3b4074908ca312e
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-03-09 12:07:02 -0500

    se crea una variable que llame el metodo System.in ( permite el ingreso de datos)

--- class1/Tarifador.java
+++ class1/Tarifador.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Tarifador{
     public static void  main (String[] args){
-        //Scanner; invoco la clase scanner, me permite que el usuario ingrese datos al programa
+        //invoco la clase scanner, me permite que el usuario ingrese datos al programa
+        Scanner scanner = new Scanner(System.in);
 
 
     }
