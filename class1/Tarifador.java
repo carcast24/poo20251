3eacb11b24a217ed44e20412d137eebba54fda17
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-03-09 12:00:44 -0500

    se quita el invocador de scanner

--- class1/Tarifador.java
+++ class1/Tarifador.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Tarifador{
     public static void  main (String[] args){
-        Scanner; // invoco la clase scanner, me permite que el usuario ingrese datos al programa
+        //Scanner; invoco la clase scanner, me permite que el usuario ingrese datos al programa
 
 
     }
