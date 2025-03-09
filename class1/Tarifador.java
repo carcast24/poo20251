c665fa3bda3e176a1aab8a6f0e1183a1a0035bff
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-03-09 12:35:34 -0500

    agrego los arrays y los inicializo en cero, tambien agregue un sistem.ouy.println, para visualizarlos

--- class1/Tarifador.java
+++ class1/Tarifador.java
@@ -1,9 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Tarifador{
     public static void  main (String[] args){
         //invoco la clase scanner, me permite que el usuario ingrese datos al programa
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        // creamos e inicializamos los Arrays como lo solicitado.
+        // 0 = puesto libre y 1 = puesto ocupado
+
+        int[] motosBajocc = new int[20]; // motos < 400cc
+        int[] motosAltocc = new int[10]; // motos >400cc
+
+        // inicializamos en 0
+
+        for (int i = 0; i < motosBajocc.length; ++i) {
+            motosBajocc[i]= 0;
+        }
+        for (int i = 0; i < motosAltocc.length; ++i);{
+            motosAltocc[i] = 0;
+        }
+
+        System.out.println(Arrays.toString(motosAltocc));
+        System.out.println(Arrays.toString(motosBajocc));
+
 
 
     }
