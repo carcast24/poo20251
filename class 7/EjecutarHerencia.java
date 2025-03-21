cf89f40998e57b5cc7bd1befe9e82ecadc9ba8b1
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-03-21 00:46:46 +0000

    creacion de consultor y ejecutar

---
+++ class 7/EjecutarHerencia.java
@@ -0,0 +1,8 @@
+public class EjecutarHerencia {
+    public static void main(String[] args) {
+        //cracion de los objetos (instanciar)
+
+        Trabajador objT1 = new Operario(135, 1, "Pepito", "gonsalez");
+        Trabajador objT2 = new Consultor(2, "gabriel", Smith, 10);
+    }
+}
