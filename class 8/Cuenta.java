cc1cf1c7b32bade1aebdbfb0d83af0aa623b5807
Carlos Andrés Castrillón <carlos.castrillonov@campusucc.edu.co>
2025-03-27 23:57:19 +0000

    clase cuenta

---
+++ class 8/Cuenta.java
@@ -0,0 +1,18 @@
+
+public class Cuenta {
+    private int numero;
+    private String tipoCuenta;
+    private double saldo;
+
+    public Cuenta(int numero, String tipoCuenta, double saldo){
+        this.numero= numero;
+        this.tipoCuenta= tipoCuenta;
+        this.saldo = saldo;
+    }
+
+    public String toString(){
+        return "Cuenta { Nombre: " + numero +
+                         "Tipo: " + tipoCuenta +
+                         "Saldo: " + saldo + "}";
+    }
+}
